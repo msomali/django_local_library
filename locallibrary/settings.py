cbe9276c937d1d884fae00ab27c6dcbea9d50af1
msomali <waleed92@live.com>
2020-04-12 22:46:37 +0300

    Added allowed heroku host

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['https://django-local-library-2020.herokuapp.com/']
+ALLOWED_HOSTS = ['django-local-library-2020.herokuapp.com']
 
 
 # Application definition
